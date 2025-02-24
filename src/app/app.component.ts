f6d76f15a35547b48a10158f3b55e354a7408c29
Fabio Altavini <f.altavini@sv-gruppe.de>
2025-02-24 16:10:50 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dummy19';
+  title = 'Angular v19';
+  loginSuccess = false;
+  loginFailed = false;
+  username: string = "";
+  password: string = "";
+  currentSsoKey: string = "";
+  ssoKeyFromA4200: string | null = "";
+
+  ngOnInit() {
+    this.ssoKeyFromA4200 = this.getSSOKey();
+    if (this.ssoKeyFromA4200) {
+      this.loginSuccess = true;
+    } else {
+      this.loginFailed = true;
+    }
+    console.log("From 4200:", this.ssoKeyFromA4200);
+  }
+
+  login() {
+    console.log("Username", this.username);
+    console.log("Passwort", this.password);
+    this.generateSSOKey();
+    console.log('SSO Key', this.currentSsoKey);
+  }
+
+  generateSSOKey() {
+    //TODO: set SSO Key (Math.random ersetzen)
+    localStorage.setItem('ssoKey', Math.random().toString(36).substr(2, 9));
+    this.currentSsoKey = Math.random().toString(36).substr(2, 9);
+  }
+
+  getSSOKey() {
+    return localStorage.getItem('ssoKey');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {InputGroup} from 'primeng/inputgroup';
+import {InputGroupAddon} from 'primeng/inputgroupaddon';
+import {FormsModule} from '@angular/forms';
+import {Card} from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputGroup,
+    InputGroupAddon,
+    FormsModule,
+    Card
   ],
   providers: [],
   bootstrap: [AppComponent]
